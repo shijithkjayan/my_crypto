75750cab52153ee1eb654ec0b11fb818dcc6aa5e
Shijith K <shijithjayan@gmail.com>
2022-09-04 15:23:49 +0530

    Added documentation

--- lib/my_crypto/messenger.ex
+++ lib/my_crypto/messenger.ex
@@ -2,6 +2,20 @@ defmodule MyCrypto.Messenger do
 
   @token System.get_env("MESSENGER_VERIFY_TOKEN")
 
+  @doc """
+  Validate the recieved token is same as what we have in the
+  system. Returns true when its same and false otherwise.
+
+  ## Examples
+
+      iex> validate_verify_token("abcd")
+      true
+
+      iex> validate_verify_token(1234)
+      false
+
+  """
+  @spec validate_verify_token(String.t()) :: boolean()
   def validate_verify_token(@token), do: true
 
   def validate_verify_token(_), do: false
