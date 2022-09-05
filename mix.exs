103a7e6ae4c8b220c047683c134413a35b90ac22
Shijith K <shijithjayan@gmail.com>
2022-09-05 22:08:31 +0530

    Upload coverage report to codecov

--- mix.exs
+++ mix.exs
@@ -11,7 +11,13 @@ defmodule MyCrypto.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
