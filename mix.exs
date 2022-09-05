352653eb9893fcb25df04522178dce233d8dd72d
Shijith K <shijithjayan@gmail.com>
2022-09-05 11:12:27 +0530

    Added coveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule MyCrypto.MixProject do
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
     ]
   end
 
@@ -39,7 +40,8 @@ defmodule MyCrypto.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tesla, "~> 1.4"},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:excoveralls, "~> 0.14.6", only: :test}
     ]
   end
 
