f85ff564cf629323b24ba3dc78adc2ec65901a32
Shijith K <shijithjayan@gmail.com>
2022-09-06 21:15:27 +0530

    Added test

--- test/my_crypto/coin_gecko_test.exs
+++ test/my_crypto/coin_gecko_test.exs
@@ -8,7 +8,7 @@ defmodule MyCrypto.CoinGeckoTest do
   alias MyCrypto.CoinGecko.HttpClientMock
 
   describe "get_prices/1" do
-    test "returns the readable dates and prices on that dates for the" <>
+    test "returns the readable dates and formats prices on that dates for the" <>
            "given coin ID when API call succeeds" do
       coin_id = "bitcoin"
       unix_dt1 = 1_661_126_400_000
@@ -18,6 +18,8 @@ defmodule MyCrypto.CoinGeckoTest do
 
       date1 = convert_unix_to_date(unix_dt1)
       date2 = convert_unix_to_date(unix_dt2)
+      usd1 = format_price(price1)
+      usd2 = format_price(price2)
 
       expect(HttpClientMock, :get_market_chart, fn ^coin_id ->
         %{
@@ -28,8 +30,50 @@ defmodule MyCrypto.CoinGeckoTest do
         }
       end)
 
-      assert ["#{date1}: #{price1} \n\n", "#{date2}: #{price2} \n\n"] ==
+      assert ["#{date1}: #{usd1} \n\n", "#{date2}: #{usd2} \n\n"] ==
                CoinGecko.get_prices(coin_id)
+    end
+
+    test "prices has a precision of 10" do
+      coin_id = "bitcoin"
+      unix_dt = 1_661_126_400_000
+      price = 21615.761693416986
+
+      date = convert_unix_to_date(unix_dt)
+      usd = format_price(price)
+
+      expect(HttpClientMock, :get_market_chart, fn ^coin_id ->
+        %{
+          "prices" => [
+            [unix_dt, price]
+          ]
+        }
+      end)
+
+      assert ["#{date}: #{usd} \n\n"] ==
+               CoinGecko.get_prices(coin_id)
+      assert 10 == usd |> String.split(".") |> List.last() |> String.length
+    end
+
+    test "prices has USD unit" do
+      coin_id = "bitcoin"
+      unix_dt = 1_661_126_400_000
+      price = 21615.761693416986
+
+      date = convert_unix_to_date(unix_dt)
+      usd = format_price(price)
+
+      expect(HttpClientMock, :get_market_chart, fn ^coin_id ->
+        %{
+          "prices" => [
+            [unix_dt, price]
+          ]
+        }
+      end)
+
+      assert ["#{date}: #{usd} \n\n"] ==
+               CoinGecko.get_prices(coin_id)
+      assert "$" <> _ = usd
     end
 
     test "returns :error with invalid coin ID" do
@@ -58,4 +102,6 @@ defmodule MyCrypto.CoinGeckoTest do
   defp convert_unix_to_date(unix_dt) do
     unix_dt |> DateTime.from_unix!(:millisecond) |> DateTime.to_date()
   end
+
+  defp format_price(price), do: Number.Currency.number_to_currency(price)
 end
